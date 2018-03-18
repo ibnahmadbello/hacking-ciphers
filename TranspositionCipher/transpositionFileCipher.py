2fd7861fff6a3b3ee81c834c9f50a621622ef266
Ibn Ahmad Bello <justforlearningcode@gmail.com>
2018-03-18 15:18:56 +0100

    transpositionFileCipher.py

--- TranspositionCipher/transpositionFileCipher.py
+++ TranspositionCipher/transpositionFileCipher.py
@@ -6,3 +6,17 @@ def main():
     inputFileName = 'frankenstein.txt'
     # Be careful! If a file with the outputFileName name already exists,
     # this program will overwrite that file.
+    outputFileName = 'frankenstein.encrypted.txt'
+    myKey = 10
+    myMode = 'encrypt' # set to 'encrypt' or 'decrypt'
+
+    # If the input file does not exist, then the program terminates early.
+    if not os.path.exists(inputFileName):
+        print('The file %s does not exist. Quitting...' % (inputFileName))
+        sys.exit()
+
+    # If the output file already exists, give the user a chance to quit.
+    if os.path.exists(outputFileName):
+        print('This will overwrite the file %s. (C)ontinue or (Q)uit?' % (outputFileName))
+
+
