03e2f6148fd579feae294b3dbb3561044f838e82
Ibn Ahmad Bello <justforlearningcode@gmail.com>
2018-04-08 17:39:01 +0100

    affineCipher.py

---
+++ AffineCiphers/affineCipher.py
@@ -0,0 +1,85 @@
+# Affine Cipher
+
+import sys, AffineCiphers.cryptomath, random
+SYMBOLS = """ !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_
+`abcdefghijklmnopqrstuvwxyz{|}~"""
+
+
+def main():
+    myMessage = """"A computer would deserve to be called intelligent if it could 
+    deceive a human into believing that it is human." - Alan Turing"""
+    myKey = 2023
+    myMode = 'encrypt'  # set to 'encrypt' or 'decrypt'
+
+    if myMode == 'encrypt':
+        translated = encryptMessage(myKey, myMessage)
+    elif myMode == 'decrypt':
+        translated = decryptMessage(myKey, myMessage)
+    print('key: %s' % (myKey))
+    print('%sed text:' % (myMode.title()))
+    print(translated)
+
+
+def getKeyParts(key):
+    keyA = key // len(SYMBOLS)
+    keyB = key % len(SYMBOLS)
+    return keyA, keyB
+
+
+def checkKeys(keyA, keyB, mode):
+    if keyA == 1 and mode == 'encrypt':
+        sys.exit('The affine cipher becomes incredibly weak when key A is '
+                 'set to 1. Choose a different key.')
+    if keyB == 0 and mode == 'encrypt':
+        sys.exit('The affine cipher becomes incredibly weak when key B is '
+                 'set to 0. Choose a different key.')
+    if keyA < 0 or keyB < 0 or keyB > len(SYMBOLS) - 1:
+        sys.exit('Key A must be greater than 0 and Key B must be between 0 and %s.'
+                 % (len(SYMBOLS) - 1))
+    if AffineCiphers.cryptomath.gcd(keyA, len(SYMBOLS)) != 1:
+        sys.exit('Key A (%s) and the symbol set size (%s) are not relatively '
+                 'prime. Choose a different key.' % (keyA, len(SYMBOLS)))
+
+
+def encryptMessage(key, message):
+    keyA, keyB = getKeyParts(key)
+    checkKeys(keyA, keyB, 'encrypt')
+    ciphertext = ''
+    for symbol in message:
+        if symbol in SYMBOLS:
+            # encrypt this symbol
+            symIndex = SYMBOLS.find(symbol)
+            ciphertext += SYMBOLS[(symIndex * keyA + keyB) % len(SYMBOLS)]
+        else:
+            ciphertext += symbol  # Just append the symbol unencrypted
+    return ciphertext
+
+
+def decryptMessage(key, message):
+    keyA, keyB = getKeyParts(key)
+    checkKeys(keyA, keyB, 'decrypt')
+    plaintext = ''
+    modInverseOfKeyA = AffineCiphers.cryptomath.findModInverse(keyA, len(SYMBOLS))
+
+    for symbol in message:
+        if symbol in SYMBOLS:
+            # decrypt this symbol
+            symIndex = SYMBOLS.find(symbol)
+            plaintext += SYMBOLS[(symIndex - keyB) * modInverseOfKeyA % len(SYMBOLS)]
+        else:
+            plaintext += symbol  # Just append the symbol undecrypted
+    return plaintext
+
+
+def getRandomKey():
+    while True:
+        keyA = random.randint(2, len(SYMBOLS))
+        keyB = random.randint(2, len(SYMBOLS))
+        if AffineCiphers.cryptomath.gcd(keyA, len(SYMBOLS)) == 1:
+            return keyA * len(SYMBOLS) + keyB
+
+
+# If affineCipher.py is run (instead of imported as a module) call
+# the main function
+if __name__ == '__main__':
+    main()
